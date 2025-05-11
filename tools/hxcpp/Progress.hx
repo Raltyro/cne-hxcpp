5cc0fd85341de6a87037d826f6d34a6adf7860f9
MAJigsaw77 <77043862+MAJigsaw77@users.noreply.github.com>
2025-05-11 16:20:57 +0300

    Update the style and alignment.

--- tools/hxcpp/Progress.hx
+++ tools/hxcpp/Progress.hx
@@ -1,27 +1,39 @@
 package;
 
-class Progress {
-	public var current:Int;
-	public var total:Int;
+class Progress
+{
+   public var current:Int;
+   public var total:Int;
 
-	public function new(inCurrent:Int, inTotal:Int) {
-		current = inCurrent;
-		total = inTotal;
-	}
+   public function new(inCurrent:Int, inTotal:Int)
+   {
+      current = inCurrent;
+      total = inTotal;
+   }
 
-	public function progress(inCurrent:Int) {
-		current += inCurrent;
-	}
+   public function getProgress():String
+   {
+      var percent = current / total;
+      var pct = Std.int(percent * 1000) / 10;
+      var progress = Std.string(pct);
 
-	public function getProgress() {
-		var percent = current / total;
-		var pct = Std.int(percent * 1000) / 10;
-		var str = Std.string(pct);
-		if (Std.int(pct) == pct) {
-			str += ".0";
-		}
-		while (str.length < 4)
-			str = " " + str;
-		return "[" + str + "%]";
-	}
+      if (Std.int(pct) == pct)
+         progress += ".0";
+
+      return progress + "%";
+   }
+
+   public function getFormattedProgress():String
+   {
+      var progress = getProgress();
+
+      var hashes = "";
+
+      while ((hashes.length + progress.length) < 7)
+         hashes += "#";
+
+      hashes = "\x1b[90;1m" + hashes + "\x1b[0m";
+
+      return "\x1b[1m[\x1b[0m" + hashes + "\x1b[33;1m" + progress + "\x1b[0m\x1b[1m]\x1b[0m";
+   }
 }
